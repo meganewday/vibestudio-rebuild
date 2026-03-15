4e8d10c65b254aade2ee3ca03a70089d2ba624fa
meganewday <37589214+meganewday@users.noreply.github.com>
2026-03-15 15:08:30 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,16 @@
-import createMDX from '@next/mdx';
-import withPWA from 'next-pwa';
+import withPWAInit from "@ducanh2912/next-pwa";
 
-/** @type {import('next').NextConfig} */
-const baseConfig = {
-  pageExtensions: ['ts', 'tsx', 'mdx'],
+const withPWA = withPWAInit({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
+
+/** @type {import('next').NextProjectConfig} */
+const nextConfig = {
+  // Your existing config options go here (e.g., reactStrictMode, etc.)
   reactStrictMode: true,
 };
 
-const withMDX = createMDX({
-  extension: /\.mdx?$/,
-});
-
-const withPWAConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
-export default withPWAConfig(withMDX(baseConfig));
-
+export default withPWA(nextConfig);
